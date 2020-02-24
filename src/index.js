c16324f82d622d33b7c8c7b5e36e174bf542be25
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-24 14:18:50 +0500

    Update scripts
    wrote a script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+	if(!array){
+		return 0;
+	}
+	if(array){
+		var min = 0;
+		for (var i = 0; i < array.length; i++) {
+			if(array[i] < min){
+				min = array[i];
+			}
+		}
+		return min;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(!array){
+		return 0;
+	}
+	if(array){
+		var max = 0;
+		for (var i = 0; i < array.length; i++) {
+			if(array[i] > max){
+				max = array[i];
+			}
+		}
+		return max;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(!array){
+		return 0;
+	}
+	if(array.length > 0){
+		var avg = 0;
+		for (var i = 0; i < array.length; i++) {
+			avg = avg + array[i];
+		}
+		return avg/array.length;
+	}else{
+		return 0;
+	}
 }
